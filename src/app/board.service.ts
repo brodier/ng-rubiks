b3711ece02b5ffe029f2212f2cbed824196019b6
Bernard Rodier <bernard.rodier@gmail.com>
2021-05-02 16:29:49 +0200

    [WIP] different test on input form

--- src/app/board.service.ts
+++ src/app/board.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { Piece } from './_model/piece.model';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,10 +10,18 @@ export class BoardService {
 
   // Load full board position
   getBoard() {
-
+    var lists = [];
+    for(let j= 0; j < 7; j++) {
+      var rowLine = [];
+      for(let i = 0; i < 20; i++) {
+        rowLine.push({value: 0, color: 'none'});
+      }
+      lists.push({id: 'pl-' + j, pieces: rowLine});
+    }
+    return lists;
   }
 
-  // observe change on board as sequential step from a define step 
+  // observe change on board as sequential step from a define step
   observeBoard(step:number) {
 
   }
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Input} from '@angular/core';
+import {NgForm} from '@angular/forms';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {CdkDropList, CdkDragDrop, CdkDrag, moveItemInArray, transferArrayItem, copyArrayItem} from '@angular/cdk/drag-drop';
 import { Piece } from '../_model/piece.model';
@@ -11,30 +12,27 @@ import { BoardService } from '../board.service';
 })
 export class BoardComponent implements OnInit {
 
-  constructor() { }
+  constructor(bdSvc: BoardService) {
+    this.lists = bdSvc.getBoard();
+  }
 
   ngOnInit(): void {
   }
+
   @ViewChild('myrule') myrule!: CdkDropList;
 
   emptyPiece = {color: 'none', value: 0};
   newPiece = {color: 'blue', value: 7};
-  lists = [
-    {id: 'pl-1', pieces: [{color: 'red', value: 1}, {color: 'red', value: 2}, {color: 'red', value: 3}, {color: 'none', value: 0},
-    this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece]},
-    {id: 'pl-2', pieces: [{color: 'blue', value: 4}, {color: 'blue', value: 5}, {color: 'blue', value: 6}, this.emptyPiece,
-    this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece, ]},
-    {id: 'pl-3', pieces: [{color: 'yellow', value: 4}, {color: 'yellow', value: 5}, {color: 'yellow', value: 6}, this.emptyPiece,
-  this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece]},
-    {id: 'pl-4', pieces:[{color: 'black', value: 4}, {color: 'black', value: 5}, {color: 'black', value: 6},this.emptyPiece,
-  this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece, this.emptyPiece]}
-  ]
-  allLists = ['pl-1','pl-2','pl-3','pl-4'];
-  myList = [{color: 'red', value: 10}];
+  lists = [{id: 'pl-0', pieces: [{color: 'none', value: 0}]}];
+
+  colors = ['red','blue','yellow','black'];
+
+  allLists = ['pl-0', 'pl-1','pl-2','pl-3','pl-4', 'pl-5','pl-6'];
+  myList: Piece[] = this.initMyList();
   addList = [{color: 'none', value: 0}];
   movePiece(event: CdkDragDrop<Piece[]>) {
     console.info("from: " + event.previousContainer.id + " at " + event.previousIndex);
-    console.info("to: " + event.container.id + " at " + event.currentIndex); 
+    console.info("to: " + event.container.id + " at " + event.currentIndex);
 
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
@@ -44,7 +42,15 @@ export class BoardComponent implements OnInit {
                         event.previousIndex,
                         event.currentIndex);
     }
+    // todo organize line to avoid long line
+  }
 
+  initMyList() {
+    let rulePieces = new Array();
+    for(let i:number = 0; i < 14; i++) {
+        rulePieces.push(this.randomPiece());
+    }
+    return rulePieces;
   }
   /** Predicate function that only allows even numbers to be dropped into a list. */
   evenPredicate(item: CdkDrag<Piece>) {
@@ -72,10 +78,43 @@ export class BoardComponent implements OnInit {
   add() {
     console.info(this.myrule);
     // this.myrule.addItem(new CdkDrag({color: 'red', value: 10});
-    this.myList.push(this.newPiece );
+    this.myList.push(this.randomPiece() );
   }
 
-  testRemoteMove() {
-    moveItemInArray(this.lists[0].pieces, 0, 2);
+  randomPiece() {
+    return {color: this.colors[Math.floor(Math.random() * 4)], value: 1 + Math.floor(Math.random() * 13) };
+  }
+
+  testRemoteMove(mfl:string, mtl:string, mfi:string, mti:string) {
+    console.info("move_from_list obj:" + mfl);
+    let fromList = this.lists[this.allLists.indexOf(mfl)].pieces;
+    let toList = this.lists[this.allLists.indexOf(mtl)].pieces;
+    let fromIndex = parseInt(mfi);
+    let toIndex = parseInt(mti);
+    if (fromList === toList) {
+      moveItemInArray(fromList, fromIndex, toIndex);
+    } else {
+      transferArrayItem(fromList, toList, fromIndex, toIndex);
+    }
+  }
+
+  sortMyList() {
+    this.myList = this.myList.sort(function(p1,p2) {
+      const colors = ['red','blue','yellow','black'];
+
+      if(p1.value < p2.value) {
+        return -1;
+      }
+      if(p1.value > p2.value) {
+        return 1;
+      }
+      if(colors.indexOf(p1.color) < colors.indexOf(p2.color)) {
+        return -1;
+      }
+      if(colors.indexOf(p1.color) > colors.indexOf(p2.color)) {
+        return 1;
+      }
+      return 0;
+    });
   }
 }
